29e3200a442749d04fcabc94cb9eddf3f36f798b
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2025-11-04 21:19:24 +0200

    Animation Motion Blur

--- FrontEnd/Website/src/pages/Home.jsx
+++ FrontEnd/Website/src/pages/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import BlurText from '../components/animations/BlurText';
 import colors from '../config/colors';
 
 const Home = () => {
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
+
   return (
     <div className="min-h-screen flex flex-col" style={{ backgroundColor: colors.mainBeige }}>
       <Navbar />
       
-      <div className="flex-1 container mx-auto px-4 py-8">
+      <div className="flex-1 container mx-auto px-4 py-8 mt-10 flex items-start justify-center" style={{color: colors.mainBlack}}>
+        <BlurText
+          text="Build ur dream"
+          delay={150}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="text-8xl font-bold text-center"
+          style={{ color: colors.mainYellow }}
+        />
       </div>
 
       <Footer />
