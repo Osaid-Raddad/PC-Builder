e21d98352f82e1960d488af04a2447b16c20f512
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2025-11-04 12:38:59 +0200

    Commiting everything

--- FrontEnd/Website/src/App.jsx
+++ FrontEnd/Website/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import AuthLayout from './layouts/AuthLayout';
 
 function App() {
- 
   return (
-    <>
+    <Router>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#242423',
+            color: '#fff',
+          },
+          success: {
+            iconTheme: {
+              primary: '#F5CB5C',
+              secondary: '#242423',
+            },
+          },
+        }}
+      />
       
-    </>
-  )
+      <Routes>
+        <Route path="/signin" element={<AuthLayout />} />
+        <Route path="/signup" element={<AuthLayout />} />
+        <Route path="/" element={<AuthLayout />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
