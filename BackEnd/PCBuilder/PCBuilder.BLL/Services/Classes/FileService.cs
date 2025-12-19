738aee2b96b4292fdd60a9bfdca42431c84ebf37
Osaid-Raddad <osaidislam1@gmail.com>
2025-12-19 16:30:30 +0200

    Add upload Many

--- BackEnd/PCBuilder/PCBuilder.BLL/Services/Classes/FileService.cs
+++ BackEnd/PCBuilder/PCBuilder.BLL/Services/Classes/FileService.cs
@@ -15,27 +15,49 @@ namespace PCBuilder.BLL.Services.Classes
             if (file == null || file.Length == 0)
                 throw new Exception("File is empty or null");
 
-            // تأكد من أن folder صالح
+           
             folder = string.IsNullOrWhiteSpace(folder) ? "default" : folder;
 
-            // اسم الملف
+            
             var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
 
-            // المسار النهائي
+           
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", folder, fileName);
 
-            // تأكد أن الفولدر موجود
+           
             var directory = Path.GetDirectoryName(filePath);
             if (!Directory.Exists(directory))
                 Directory.CreateDirectory(directory);
 
-            // رفع الملف
+            
             using (var stream = File.Create(filePath))
             {
                 await file.CopyToAsync(stream);
             }
 
             return fileName;
+        }
+
+        public async Task<List<string>> UploadManyAsync(List<IFormFile> files)
+        {
+            var fileNames = new List<string>();
+            foreach (var file in files)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Posts", fileName);
+
+                    using (var stream = File.Create(filePath))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    fileNames.Add(fileName);
+                }
+            }
+
+            return fileNames;
+
         }
     }
 }
