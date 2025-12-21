30a4b424abe1d0baf333e6337fa92ec148d076de
Osaid-Raddad <osaidislam1@gmail.com>
2025-12-21 15:56:00 +0200

    make get all user public

---
+++ BackEnd/PCBuilder/PCBuilder.PL/Controllers/PublicController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PCBuilder.BLL.Services.Interfaces;
+
+namespace PCBuilder.PL.Controllers
+{
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    [Area("Public")]
+    [Authorize(Roles ="Admin,SuperAdmin,User,TechSupport")]
+    public class PublicController : ControllerBase
+    {
+        private readonly IUserService _userservice;
+
+        public PublicController(IUserService userservice)
+        {
+            _userservice = userservice;
+        }
+
+        
+        [HttpGet("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userservice.GetAllUsersAsync();
+            return Ok(users);
+        }
+    }
+}
